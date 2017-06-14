950c0de67114554f2eeb8aed957a2500146c6926
voffoma <offoma.victoria@gmail.com>
2017-06-14 22:51:39 +0100

    create user model and migration

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
