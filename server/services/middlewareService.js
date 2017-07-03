009d474deadfea9418c2685786ac2fa0554c897e
voffoma <offoma.victoria@gmail.com>
2017-07-03 23:32:53 +0100

    Add middleware function that verifies token

--- server/services/middlewareService.js
+++ server/services/middlewareService.js
@@ -1,6 +1,20 @@
 const models = require('../models/index');
+const jwt = require('jsonwebtoken');
 
 module.exports = () => {
+  const verifyToken = (req, res, next) => {
+    const token = req.body.token || req.params.token || req.headers['x-access-token'];
+    jwt.verify(token, 'VOR4MA.1', (error, decoded) => {
+      if (error) {
+        req.user = undefined;
+        res.status(400).send({ success: false, message: 'token authentication unsuccessful' });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  };
+
   const isUserExisting = (req, res, next) => {
     models.User.find({
       where: {
@@ -19,6 +33,6 @@ module.exports = () => {
   };
 
   return {
-    isUserExisting
+    verifyToken, isUserExisting
   };
 };
