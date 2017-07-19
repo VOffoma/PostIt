294113266ff5eb1a772014289e958ab9bda4f204
voffoma <offoma.victoria@gmail.com>
2017-07-19 10:40:48 +0100

    delete old migrations

--- server/app.js
+++ server/app.js
@@ -8,14 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const apiRoutes = require('./routes')();
-// const userRoutes = require('./routes/userRoutes')();
-// const groupRoutes = require('./routes/groupRoutes')();
-// const middlewareService = require('./services/middlewareService')();
 
-
-// app.use('/api/user', userRoutes);
-// app.use(middlewareService.verifyToken);
-// app.use('/api/group', groupRoutes);
 app.use('/api', apiRoutes);
 app.listen(port, () => {
   console.log(`i m listening on port ${port}`);
